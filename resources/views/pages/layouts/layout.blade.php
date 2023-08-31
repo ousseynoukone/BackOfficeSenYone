3ae49043bb310e37fd6db90c679d13c1ad0954fb
ousseynoukone <ousseynou781227@gmail.com>
2023-08-31 21:16:40 +0000

    yeah

--- resources/views/pages/layouts/layout.blade.php
+++ resources/views/pages/layouts/layout.blade.php
@@ -164,9 +164,9 @@
                 <div class="btn" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown">{{ Auth::user()->name }} <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg></div>
               <li class="nav-item dropdown">
 
-                <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"   
+                <a class="nav-link nav-icon-hover"  
                   aria-expanded="false">
-                  <img data-bs-toggle="dropdown" src="{{asset("assets/images/profile/user-1.jpg")}}" alt="" width="35" height="35" class="rounded-circle">
+                  <img  src="{{asset("assets/images/profile/user-1.jpg")}}" alt="" width="35" height="35" class="rounded-circle">
                 </a>
                 
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
