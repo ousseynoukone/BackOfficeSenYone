2f0954ac5c089f465e4c17b8dc8b1ae5547eca38
ousseynoukone <ousseynou781227@gmail.com>
2023-12-25 13:44:37 +0000

    optimizaton

--- app/Http/Controllers/API/Operations/Helpers/BusStopFinder.php
+++ app/Http/Controllers/API/Operations/Helpers/BusStopFinder.php
@@ -10,74 +10,89 @@ class BusStopFinder {
     }
 
     public function getNearestBusStop($latitude, $longitude,$limit) {
-        // API endpoint
-        $apiEndpoint = 'https://api.geoapify.com/v2/places';
+        // // API endpoint
+        // $apiEndpoint = 'https://api.geoapify.com/v2/places';
 
-        // API parameters
-        $categories = 'public_transport';
-        $bias = "proximity:$longitude,$latitude";
-        $limit = $limit;
+        // // API parameters
+        // $categories = 'public_transport';
+        // $bias = "proximity:$longitude,$latitude";
+        // $limit = $limit;
 
-        // Build the API request URL
-        $apiUrl = "$apiEndpoint?categories=$categories&bias=$bias&limit=$limit&apiKey=$this->apiKey";
+        // // Build the API request URL
+        // $apiUrl = "$apiEndpoint?categories=$categories&bias=$bias&limit=$limit&apiKey=$this->apiKey";
 
-        // Make the API request
-        $response = file_get_contents($apiUrl);
+        // // Make the API request
+        // $response = file_get_contents($apiUrl);
 
-        // Check if the request was successful
-        if ($response === false) {
-            // Handle error (you might want to log or throw an exception)
-            return null;
-        }
+        // // Check if the request was successful
+        // if ($response === false) {
+        //     // Handle error (you might want to log or throw an exception)
+        //     return null;
+        // }
 
-        // Parse the JSON response
-        $data = json_decode($response, true);
+        // // Parse the JSON response
+        // $data = json_decode($response, true);
 
-        // Check if JSON decoding was successful
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            // Handle JSON decoding error
-            return null;
-        }
+        // // Check if JSON decoding was successful
+        // if (json_last_error() !== JSON_ERROR_NONE) {
+        //     // Handle JSON decoding error
+        //     return null;
+        // }
 
 
-        // Check if the response contains any places
-        if (isset($data['features'][0])) {
-            // Extract the relevant information
-            $properties = $data['features'][0]['properties'];
-            $geometry = $data['features'][0]['geometry']['coordinates'];
+        // // Check if the response contains any places
+        // if (isset($data['features'][0])) {
+        //     // Extract the relevant information
+        //     $properties = $data['features'][0]['properties'];
+        //     $geometry = $data['features'][0]['geometry']['coordinates'];
 
 
     
 
-            $street ="Adresse inconnu";
+        //     $street ="Adresse inconnu";
 
-            // Extracted information
-            $coordinates = ['lon' => $geometry[0], 'lat' => $geometry[1]];
-            $distance = $properties['distance'];
-            // $operator = $properties['datasource']['raw']['operator'];
-            if( isset($properties['street'])){
-                $street = $properties['street'];
+        //     // Extracted information
+        //     $coordinates = ['lon' => $geometry[0], 'lat' => $geometry[1]];
+        //     $distance = $properties['distance'];
+        //     // $operator = $properties['datasource']['raw']['operator'];
+        //     if( isset($properties['street'])){
+        //         $street = $properties['street'];
 
-            }else if(isset($properties['road'])){
-                $street = $properties['road'];
+        //     }else if(isset($properties['road'])){
+        //         $street = $properties['road'];
 
-            }else{
-                $street = $properties['suburb'];
+        //     }else{
+        //         $street = $properties['suburb'];
 
-            }
+        //     }
 
-            // Return the result
-            return [
+        //     // Return the result
+        //     return [
+        //         'coordinates' => $coordinates,
+        //         'distance' => $distance,
+        //         // 'operator' => $operator,
+        //         'street' => $street,
+        //     ];
+
+        // } else {
+        //     // No bus stops found
+        //     return null;
+        // }
+
+   
+
+        $coordinates = [
+            "lon" => -17.450774799999998,
+            "lat" => 14.682099399869736
+        ];
+
+
+                   return [
                 'coordinates' => $coordinates,
-                'distance' => $distance,
+                'distance' => 0,
                 // 'operator' => $operator,
-                'street' => $street,
+                'street' => "none",
             ];
-
-        } else {
-            // No bus stops found
-            return null;
-        }
     }
 
 
